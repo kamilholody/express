4bcfdd5f5246009f24c072ec1e5b0f48acfc12e6
Kamil Hołody <kamilholody91@gmail.com>
2020-12-12 14:14:08 +0100

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    db: 'mongodb+srv://admin:Vy31il7z8wLRoCug@cluster0.7curn.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+
+}
