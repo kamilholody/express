8cf4789274f0aa45798b1fd905fe7e8170e3d3cb
Kamil Hołody <kamilholody91@gmail.com>
2020-12-14 21:27:19 +0100

    quiz form

--- models/news.js
+++ models/news.js
@@ -1,7 +1,7 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var newsSchema = new Schema({
+const newsSchema = new Schema({
     title: {
         type: String,
         required: [true, 'Pole tytułu jest wymagane']
